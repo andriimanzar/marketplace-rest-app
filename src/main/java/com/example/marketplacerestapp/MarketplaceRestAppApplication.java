1b883b1cadc613d54eae6c64625540097e3410c5
andrii_manzar <manzar20011@gmail.com>
2022-07-22 23:33:32 +0300

    Initial commit

---
+++ src/main/java/com/example/marketplacerestapp/MarketplaceRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.marketplacerestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceRestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketplaceRestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/marketplacerestapp/MarketplaceRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marketplacerestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceRestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
